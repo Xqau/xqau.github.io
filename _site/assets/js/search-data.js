5b5a1eb49864dfc913bbcad1b81f01cfb779e5d5
xqau <janikrishna2005@gamil.com>
2022-05-20 12:33:12 +0530

    Changes

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Hello Internet!",
+        "excerpt":"Hello Internet This is my first post ! ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2022/05/20/welcome-to-jekyll.html"
+      }]
